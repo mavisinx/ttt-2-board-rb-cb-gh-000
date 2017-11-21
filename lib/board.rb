df9d58272776a5c48685ca927a533ad7baec75bd
Mai Nguyen <m.nguyen-17@student.lboro.ac.uk>
2017-11-21 21:02:21 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,2 @@
 # Define the variable board below.
-row = [" ", " ", " "]
-
-# Move X to the Position 2, Middle
-row[1] = "X"
-row #=> [" ", "X", " "]
+board = [  " ", " ", " ", " ", " ", " ", " ", " ", " "]
